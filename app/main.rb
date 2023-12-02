68c56d70be995bc3df8cd3d89b001564d703f205
osawa-koki <koki@osawa.dev>
2023-12-02 17:17:42 +0900

    マルチスレッド実行。

--- app/main.rb
+++ app/main.rb
@@ -3,9 +3,13 @@
 require 'benchmark'
 
 require './app/simple'
+require './app/multi_thread'
 
 Benchmark.bm 10 do |r|
   r.report 'simple' do
     simple_execute
+  end
+  r.report 'multi_thread' do
+    multi_thread_execute
   end
 end
