cd5abf76f810356aa15c928f6f1b1c70dccdb592
osawa-koki <koki@osawa.dev>
2023-12-02 17:16:39 +0900

    rubocop setting

--- app/simple.rb
+++ app/simple.rb
@@ -6,7 +6,7 @@ require 'pathname'
 
 Dotenv.load
 
-def simple_execute # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
+def simple_execute
   aspect_ratio = ENV['ASPECT_RATIO'].to_f
 
   filesize_width = ENV['FILESIZE_WIDTH'].to_i
